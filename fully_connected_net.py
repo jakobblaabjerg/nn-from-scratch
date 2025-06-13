fix: update backprop

fully_connected_net.py
@@ -6,17 +6,18 @@ from src.loss.functions import LossFuncs
 
 class FullyConnectedNet():
    
-    def __init__(self, n_layers, n_hidden, n_input, n_output):
+    def __init__(self, n_hidden_layers, n_hidden_units, n_input, n_output):
 
-        self.n_layers = n_layers
-        self.n_hidden = n_hidden
+        self.n_hidden_layers = n_hidden_layers
+        self.n_hidden_units = n_hidden_units
         self.n_output = n_output
         self.n_input = n_input
+        self.n_layers = self.n_hidden_layers+2
 
         self.weights = {}
         self.biases = {}
 
-        layer_sizes = [n_input] + [self.n_hidden] * self.n_layers + [self.n_output]
+        layer_sizes = [n_input] + [self.n_hidden_units] * self.n_hidden_layers + [self.n_output]
 
         for i in range(len(layer_sizes) - 1):
 
@@ -41,9 +42,9 @@ class FullyConnectedNet():
         A_prev = X
         self.A['A0'] = A_prev
 
-        total_layers = self.n_layers+1
+        
 
-        for i in range(total_layers):
+        for i in range(self.n_layers-1):
 
             W = self.weights[f'W{i+1}']
             b = self.biases[f'b{i+1}']
@@ -51,7 +52,7 @@ class FullyConnectedNet():
             Z = np.matmul(W, A_prev)+b
             self.Z[f'Z{i+1}'] = Z
 
-            if i+1 < total_layers:
+            if i+1 < self.n_layers-1:
                 A = Activations.relu(Z)
             else:
                 A = Activations.softmax(Z)
@@ -79,19 +80,24 @@ class FullyConnectedNet():
 
     def back_prop(self, y):
 
-        dz4 = A4-y
-        dw4 = dz4*A3
-        db4 = 1*dz4
-        da3 = w4*dz4
-        dz3 = da3* (z3>0)
+        m = y.shape[1]
+        dZ_next = self.A[f"A{self.n_layers-1}"]-y
+        self.dZ[f"dZ{self.n_layers-1}"] = dZ_next
 
+        for i in reversed(range(self.n_layers-1)):
+          
+            W_next = self.W[f"W{i+1}"]
+            dA = np.matmul(W_next.T, dZ_next)           
+            dZ = dA*(self.Z[f"Z{i}"]>0) # no matrix multiplication, because we did an elementwise derivative
 
+            dW_next = np.matmul(dZ_next, self.A[f"A{i}"].T)/m
+            db_next = np.sum(dZ_next, axis=1, keepdims=True)/m
 
-
-
-
-
-        pass
+            # self.dA[f"dA{i}"] = dA
+            # self.dZ[f"dZ{i}"] = dZ
+            self.dW[f"dW{i+1}"] = dW_next
+            self.db[f"dB{i+1}"] = db_next
+            dZ_next = dZ
 
 
 
@@ -110,3 +116,25 @@ if __name__ == "__main__":
     y_hat = params[-1]['A4']
     loss = LossFuncs.categorical_cross_entropy(y_hat, y)
     print(loss)
+
+
+
+
+
+
+
+        #     dZ4 = A4-y
+        # dW4 = dZ4*A3
+        # db4 = 1*dZ4
+        # dA3 = W4*dZ4
+        # dZ3 = dA3 * (Z3>0)
+        # dW3 = dZ3 * A2 
+        # db3 = dZ3 *1
+        # dA2 = W3 * dZ3
+        # dZ2 = dA2 * (Z2>0)
+        # dW2 = dZ2*A1
+        # db2 = dZ2 * 1
+        # dA1 = W2 * dZ2
+        # dZ1 = dA1* (Z1 > 0)
+        # dW1 = dZ1 * X
+        # db1 = dZ1 * 1